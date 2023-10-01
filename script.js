0aecf4da53c5c834d3a26964c0f9e6fca9530966
rohan7745 <138517784+rohan7745@users.noreply.github.com>
2023-10-02 02:35:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+// Find the element with the id "level"
+const targetElement = document.getElementById("level");
+
+if (targetElement) {
+    let domLevel = 0;
+    let currentNode = targetElement;
+
+    // Traverse up the DOM tree until we reach the root (html) element
+    while (currentNode !== document.documentElement) {
+        domLevel++;
+        currentNode = currentNode.parentElement;
+    }
+
+    // Display the DOM level using the alert() function
+    alert("The level of the element is: " + domLevel);
+} else {
+    alert("Element with id 'level' not found.");
+}
+
